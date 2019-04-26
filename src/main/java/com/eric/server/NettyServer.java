58a244ff26a04292689edea284f7cb9d1c8a66f2
EricWord <2635863525@qq.com>
2019-04-26 15:09:31 +0800

    完成项目代码的编写

---
+++ src/main/java/com/eric/server/NettyServer.java
@@ -0,0 +1,40 @@
+package com.eric.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * @author  eric
+ */
+public class NettyServer {
+    public static void main(String[] args) {
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+        //对应传统IO编程中的接收新连接的线程 ，主要负责创建新的连接
+        NioEventLoopGroup boos = new NioEventLoopGroup();
+        //对应传统 IO编程中的负责读取数据的线程，主要用于读取数据以及业务逻辑处理
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        serverBootstrap
+                .group(boos, worker)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    protected void initChannel(NioSocketChannel ch) {
+                        ch.pipeline().addLast(new StringDecoder());
+                        ch.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext ctx, String msg) {
+                                System.out.println(msg);
+                            }
+                        });
+                    }
+                })
+                .bind(8000);
+    }
+
+}
